c8564bc96281d3131735b41891bb1c012cf0c682
Yasir Raees <typelikepro@example.com>
2026-01-25 04:42:37 +0500

    Feat: Add GitHub contribution graph

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
+import GitHubActivity from "./components/homepage/github-activity";
 
 async function getData() {
   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`, {
@@ -36,6 +37,7 @@ export default async function Home() {
       <Experience />
       <Skills />
       <Projects />
+      <GitHubActivity />
       <Education />
       <Blog blogs={blogs} />
       <ContactSection />
